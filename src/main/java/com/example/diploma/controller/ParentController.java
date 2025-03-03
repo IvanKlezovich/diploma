80c7f6a52f1447bf32c78e3919c8095207eb04d5
Ivan Klezovich <ivan_kliazovich@senla.eu>
2025-03-03 22:14:15 +0300

    dairy-functional: add some scripts for view part 2

--- src/main/java/com/example/diploma/controller/ParentController.java
+++ src/main/java/com/example/diploma/controller/ParentController.java
@@ -1,42 +1,43 @@
-//package com.example.diploma.controller;
-//
-//import com.example.diploma.entity.Parent;
-//import com.example.diploma.entity.Student;
-//import com.example.diploma.service.ParentService;
-//import java.util.List;
-//import java.util.UUID;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/parent")
-//@RequiredArgsConstructor
-//public class ParentController {
-//
+package com.example.diploma.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/parent")
+@RequiredArgsConstructor
+public class ParentController {
+
 //  private final ParentService parentService;
-//
-//  @GetMapping("/{studentId}")
-//  public List<Parent> getParent(@PathVariable UUID studentId) {
-//    return null;
-//  }
-//
-//  @GetMapping("/children/{parentId}")
-//  public List<Student> getChildren(@PathVariable UUID parentId) {
-//    return null;
-//  }
-//
-//  @DeleteMapping("/delete")
-//  public void deleteParent(@PathVariable UUID parentId) {
-//
-//  }
-//
-//  @PostMapping("/add/children")
-//  public Parent addChildren(Student student) {
-//    return null;
-//  }
-//}
+
+  @RequestMapping("/")
+  public String index() {
+    return "parent/index";
+  }
+
+  @RequestMapping("/events")
+  public String events() {
+    return "parent/events";
+  }
+
+  @RequestMapping("/grades")
+  public String grades() {
+    return "parent/grades";
+  }
+
+  @RequestMapping("/homework")
+  public String homework() {
+    return "parent/homework";
+  }
+
+  @RequestMapping("/materials")
+  public String materials() {
+    return "parent/materials";
+  }
+
+  @RequestMapping("/notes")
+  public String notes() {
+    return "parent/notes";
+  }
+}
